9818fea2f4ed0af30af95f5d4efbc0ae0bb797bd
Vicente Carreño Villagrán <53504276+VicenteCV@users.noreply.github.com>
2019-08-20 18:57:25 -0400

    Add files via upload
    Se define función y se imprime fuera de esta, para demostrar que, al no ser llamada la función, esta no es activada y no se imprime el interior de la función.

---
+++ 20082019/000249.py
@@ -0,0 +1,8 @@
+#Que son las funciones
+#Comentarios
+#Utilizados para generar comentarios del codigo
+#Ejemplo 1
+def Funcion1():
+	print 'AHHH'
+	print 'Ahhhhhaaahhh'
+print 'esto es fuera de la funcion'
