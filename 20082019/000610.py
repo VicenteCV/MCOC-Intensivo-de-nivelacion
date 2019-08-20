8f922c8725c2774b97cfb5941a6a7ae1ad82be0f
Vicente Carreño Villagrán <53504276+VicenteCV@users.noreply.github.com>
2019-08-20 18:59:05 -0400

    Add files via upload
    Empieza ejemplo 2 donde se usa función que demanda una variable que será alterada dentro de la función. Luego se asigna el valor cambiado a otra variable fuera de la función.

---
+++ 20082019/000610.py
@@ -0,0 +1,16 @@
+#Que son las funciones
+#Comentarios
+#Utilizados para generar comentarios del codigo
+#Ejemplo 1
+print 'Ejemplo 1'
+def Funcion1():
+	print 'AHHH'
+	print 'Ahhhhhaaahhh'
+print 'esto es fuera de la funcion'
+Funcion1()
+#Ejemplo 2
+print 'Ejemplo 2'
+def  Funcion2(x):
+	return 3*x
+a= Funcion2(5)
+print a
