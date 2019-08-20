cd597b65b8b04228fe9c30e133d6de447cb8733b
Vicente Carreño Villagrán <53504276+VicenteCV@users.noreply.github.com>
2019-08-19 21:22:43 -0400

    Add files via upload
    Final de ejemplo 2 con cambio de variables para mostrar funcionalidad de else.

---
+++ 19082019/001132.py
@@ -0,0 +1,18 @@
+#If else statements
+#Inicio de ejemplos
+print 'Ejemplo 1'
+a=6
+b=7
+if a <b:
+	print 'a es menor que b'
+	print 'a es definitivamente menor que b'
+print 'No estoy seguro que a sea menor que b'
+#ejemplo 2
+print 'Ejemplo 2'
+c=5
+d=4
+if c<d:
+	print 'c es menor que d'
+else:
+	print 'c NO es menor que d'
+print 'Fuera del ciclo if'
