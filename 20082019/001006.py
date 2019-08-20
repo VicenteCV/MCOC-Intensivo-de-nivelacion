eabd6259bcfeb01b27a79d6f7eca1ad59cb974b0
Vicente Carreño Villagrán <53504276+VicenteCV@users.noreply.github.com>
2019-08-20 19:20:02 -0400

    Add files via upload
    Ejemplo 4. Se define una función sin 'return'. Esta es llamada y se imprime lo que está dentro de la función.

---
+++ 20082019/001006.py
@@ -0,0 +1,31 @@
+#Que son las funciones
+#Comentarios
+#Utilizados para generar comentarios del codigo
+#Ejemplo 1
+print 'Ejemplo 1'
+def Funcion1():
+	print 'AHHH'
+	print 'Ahhhhhaaahhh'
+print 'esto es fuera de la funcion'
+Funcion1()
+#Ejemplo 2
+print 'Ejemplo 2'
+def  Funcion2(x):
+	return 3*x
+a= Funcion2(5)
+b= Funcion2(7)
+print a
+print b
+#Ejemplo 3
+print 'Ejemplo 3'
+def Funcion3(x,y):
+	return x**y
+c= Funcion3(2,3)
+print c
+#Ejemplo 4
+print 'Ejemplo 4'
+def Funcion4(x):
+	print x
+	print 'Holaaaa'
+
+Funcion4(8)
