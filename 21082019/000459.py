2e8b867ab0f6875d7e98953b42da3c8b25f723d4
Vicente Carreño Villagrán <53504276+VicenteCV@users.noreply.github.com>
2019-08-21 23:01:44 -0400

    Add files via upload
    Se remueve último valor agregado a lista y se imprime. Se localiza el primer objeto en la lista y se imprime.

---
+++ 21082019/000459.py
@@ -0,0 +1,17 @@
+#3ra Entrega
+#Listas
+#Ejemplo 1
+print 'Ejemplo 1'
+a=[3,10,-1]
+print a
+a.append(1)
+print a
+a.append('holaa')
+print a
+a.append([1,2])
+print a
+a.pop()
+print a
+a.pop()
+print a
+print a[0]
