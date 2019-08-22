98b51174af7cfcd806209100565a7ff4338377e7
Vicente Carreño Villagrán <53504276+VicenteCV@users.noreply.github.com>
2019-08-21 23:04:47 -0400

    Add files via upload
    Se cambia un valor dentro de la lista y se imprime para comprobar.

---
+++ 21082019/000557.py
@@ -0,0 +1,20 @@
+#3ra Entrega
+#Listas
+#Ejemplo 1
+print 'Ejemplo 1'
+a=[3,10,-1]
+print a
+a.append(1)
+print a
+a.append('holaa')
+print a
+a.append([1,2])
+print a
+a.pop()
+print a
+a.pop()
+print a
+print a[0]
+print a[3]
+a[0]=100
+print(a)
