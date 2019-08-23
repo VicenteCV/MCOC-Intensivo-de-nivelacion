78a7cb875ec1130f5771512221a140d85a3ad4d9
Vicente Carreño Villagrán <53504276+VicenteCV@users.noreply.github.com>
2019-08-22 22:40:03 -0400

    Add files via upload
    Se introduce el ciclo for para recorrer lista.

---
+++ 22082019/000200.py
@@ -0,0 +1,7 @@
+#4ta Entrega
+#Ciclo For
+#Ejemplo 1
+print 'Ejemplo 1'
+a=['Bananaaaa!!!','Bee do Bee do Bee do','Tu le bella comme le papaya']
+for i in a: #Ciclo for que recorre lista
+	print i #Al recorrer la lista elemento, por elemento, estos se imprimen durante cada ciclo.
