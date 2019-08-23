e0c8c900e93567f3f2bb1662e0ad448634660381
Vicente Carreño Villagrán <53504276+VicenteCV@users.noreply.github.com>
2019-08-22 23:04:11 -0400

    Add files via upload
    Ejemplo 3. Introducción de range. se utiliza ciclo for en un rango especifico y se imprime suma de valores en ese rango.

---
+++ 22082019/000602.py
@@ -0,0 +1,24 @@
+#4ta Entrega
+#Ciclo For
+#Ejemplo 1
+print 'Ejemplo 1'
+a=['Bananaaaa!!!','Bee do Bee do Bee do','Tu le bella comme le papaya']
+for i in a: #Ciclo for que recorre lista
+	print i #Al recorrer la lista elemento, por elemento, estos se imprimen durante cada ciclo.
+#Ejemplo 2
+print 'Ejemplo 2'
+print 'Bienvenidos a la historia del club mas grande de Chile. UCDC'
+b=[1997,2002,2005,2010,2016,2016,2018]
+total=0
+for j in b:
+	total=total + j
+print total
+#Ejemplo 3
+range(1,5)
+c=list(range(1,5))
+print c
+total2=0
+for k in range(1,5):
+	print k
+	total2+=k
+print total2
