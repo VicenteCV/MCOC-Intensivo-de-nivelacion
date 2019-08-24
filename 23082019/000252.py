21628d7263393fae10e719fcfc1bab0005333f60
Vicente Carreño Villagrán <53504276+VicenteCV@users.noreply.github.com>
2019-08-23 20:07:46 -0400

    Add files via upload
    Ejemplo 1. Introducción en Ciclo while

---
+++ 23082019/000252.py
@@ -0,0 +1,10 @@
+#5ta entrega
+#Ciclos while
+#Ejemplo 1
+print 'Ejemplo 1'
+Total=0
+i=1
+while i<10: #Luego de varirar i repetidas veces, se verifica si i sigue siendo menor a 10, si es asi, se vuelve a entrar al ciclo.
+	Total+=i # Se agrega el valor de i al total 
+	i+=1 # Se agrega 1 al valor de i
+print Total
