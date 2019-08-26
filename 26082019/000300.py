8adf19bf9fc8e822fcd3aa1b5be918fde0ad8ce0
Vicente Carreño Villagrán <53504276+VicenteCV@users.noreply.github.com>
2019-08-26 15:24:37 -0400

    Add files via upload
    Ejemplo 1. Se crea diccionario y se anexan entradas y valores en conjunto.

---
+++ 26082019/000300.py
@@ -0,0 +1,8 @@
+#6ta entrega
+#Diccionarios
+a={} #Se crea diccionario vacio
+a['Jorge, el curioso']=24 #Se anexa una entrada al diccionario junto con un valor
+a['Thomas, the steam engine']=30
+a['Jenny, Pablos ex']=29
+print a #Se imprime diccionario
+print a['Jorge, el curioso'] #Se imprime valor asociado a entrada en diccionario
