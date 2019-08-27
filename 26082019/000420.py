d0f23d00259e15734c73251d6269a42f2727b188
Vicente Carreño Villagrán <53504276+VicenteCV@users.noreply.github.com>
2019-08-26 21:26:26 -0400

    Add files via upload
    Segundo ejemplo. Llave de diccionario es un número.

---
+++ 26082019/000420.py
@@ -0,0 +1,17 @@
+#6ta entrega
+#Diccionarios
+#Ejemplo 1
+print 'Ejemplo 1'
+a={} #Se crea diccionario vacio
+a['Jorge, el curioso']=24 #Se anexa una entrada al diccionario junto con un valor
+a['Thomas, the steam engine']=30
+a['Jenny, Pablos ex']=29
+print a #Se imprime diccionario
+print a['Jorge, el curioso'] #Se imprime valor asociado a entrada en diccionario
+a['Jenny, Pablos ex']=22
+print a['Jenny, Pablos ex']
+#Ejemplo 2
+print 'Ejemplo 2'
+b={}
+b[15]=51 #Se pueden tener llaves con numeros
+print b
