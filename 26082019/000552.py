5da6f754df7e50134a95929aafc30cd7c327c028
Vicente Carreño Villagrán <53504276+VicenteCV@users.noreply.github.com>
2019-08-26 21:35:22 -0400

    Add files via upload
    Ejemplo 3. Se recorre pareja llave-valor del diccionario usando ciclo for.

---
+++ 26082019/000552.py
@@ -0,0 +1,23 @@
+#6ta entrega
+#Diccionarios
+#Ejemplo 1
+print 'Ejemplo 1'
+a={} #Se crea diccionario vacio
+a['Jorge, el curioso']=24 #Se anexa una entrada al diccionario junto con un valor
+a['Thomas, the steam engine']=30
+a['Jenny, Pablos ex']=29
+print a #Se imprime diccionario
+print a['Jorge, el curioso'] #Se imprime valor asociado a entrada en diccionario
+a['Jenny, Pablos ex']=22
+print a['Jenny, Pablos ex']
+#Ejemplo 2
+print 'Ejemplo 2'
+b={}
+b[15]=51 #Se pueden tener llaves con numeros
+print b
+#Ejemplo 3
+print 'Ejemplo 3'
+print 'Se utiliza diccionario de primer ejemplo'
+for i,j in a.items(): #Ciclo for que recorre parejas de valor y llave en diccionario
+	print 'La llave es: ',i
+	print 'Valor asociado a dicha llave es: ',j
