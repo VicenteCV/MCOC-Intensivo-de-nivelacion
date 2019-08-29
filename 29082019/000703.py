6014ac16366d95945d7814b9895bb53a29f31a12
Vicente Carreño Villagrán <53504276+VicenteCV@users.noreply.github.com>
2019-08-29 15:21:31 -0400

    Add files via upload
    Ejemplo 1

---
+++ 29082019/000703.py
@@ -0,0 +1,28 @@
+#9na entrega
+#Tutorial completo de numpy
+#Ejemplo 1
+print 'Ejemplo 1'
+a=[1,2,3]
+b=[5,4,3]
+c=a+b #Se crea llista conteniendo ambas listas a y b
+print c
+salida=[]
+for i,j in zip(a,b): #Recorre ambas listas simultaneamente
+	salida.append(i+j)
+print salida
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
