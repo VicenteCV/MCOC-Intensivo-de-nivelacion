fc49ed516daba0c2fef3be07137536f8ef435e29
Vicente Carreño Villagrán <53504276+VicenteCV@users.noreply.github.com>
2019-08-28 23:08:28 -0400

    Add files via upload
    Ejemplo de usos de numpy para crear, analizar y cambiar arrays.

---
+++ 28082019/000925.py
@@ -0,0 +1,18 @@
+#8va entrega
+#Arrays con numpy
+#Ejmeplo 1
+print 'Ejmeplo 1'
+import numpy as np
+a=np.array([4,3,2]) #Crea array con lista introducida
+print a
+b=np.arange(1,12,2) #Crea array desde 1 a 12 con pasos de 2
+print b
+c=np.linspace(1,12,6) #Crea array de 6 elementos, entre 1 y 12 con valores float espaciado de forma pareja
+print c
+d=c.reshape(3,2) #Cambia la forma del array. 3 filas de 2 elementos cada una
+print d
+e=d.size #arroja el tamano o cantidad de elementos del array
+print e
+#Ejemplo 2
+print 'Ejemplo 2'
+
